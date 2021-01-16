bd756384ccff6a68aac938d8dd738e909bf707c6
Glenn <glenn@rudgesoftware.com>
2021-01-16 09:54:04 -0500

    [HephaestusOption] UI - Added navigation to landing page, with styles.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,9 @@
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
     <!-- Google Fonts Roboto -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Fraunces:wght@100;400;700&display=swap" rel="stylesheet">
+
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="{{ secure_asset('css/bootstrap.min.css') }}">
     <!-- Material Design Bootstrap -->
@@ -23,16 +25,46 @@
 </head>
 
 <body class="skin-light">
-
 <!-- Start your project here-->
-<div style="height: 100vh">
+<!--Navbar-->
+<nav class="navbar navbar-expand-lg navbar-light fixed-top scrolling-navbar">
+    <div class="container">
+        <a id="navbarBrand" class="navbar-brand" href="#"><strong>Hephaestus Option</strong></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7"
+                aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-7">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <div>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Register<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Login<span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<div id="landingContent" style="height: 100vh">
     <div class="flex-center flex-column">
-        <img id="landing-logo" src="{{ secure_url('img/logo.jpg') }}" alt="">
-        <h1 class="text-hide animated fadeIn mb-4" style="width: 250px; height: 90px;">Hephaestus Option</h1>
-{{--        <a href="{{ secure_asset('img/logo.jpg') }}">Click Me</a>--}}
-        <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
+        <img class="p-0" id="landing-logo" src="{{ secure_url('img/logo.jpg') }}" alt="">
+        <p class="animated fadeIn mb-3 text-dark">Thank you for using our product. We're glad you're with us.</p>
 
-        <p class="animated fadeIn text-muted">Rudge Software Team</p>
+        <p class="animated fadeIn lead">Rudge Software Team</p>
     </div>
 </div>
 <!-- End your project here-->
